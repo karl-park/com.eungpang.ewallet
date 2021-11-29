0e0ce3016ee34e5a8bcc626b0900540f186df2d5
KarlPark <karl.panki.park@gmail.com>
2021-11-29 19:09:02 +0800

    Add dependencies and configure the project to use Kotlin DSL for dependency declaration

---
+++ android/buildSrc/src/main/java/com/eungpang/buildsrc/Dependencies.kt
@@ -0,0 +1,101 @@
+package com.eungpang.buildsrc
+
+object Versions {
+    const val applicationId = "com.eungpang.ewallet"
+
+    const val compileSdkVersion = 31
+    const val minSdkVersion = 31
+    const val targetSdkVersion = 31
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.3"
+
+    object Kotlin {
+        private const val version = "1.6.0"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+
+        object Coroutines {
+            private const val version = "1.5.2"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        }
+    }
+
+    object AndroidX {
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        }
+
+        const val coreKtx = "'androidx.core:core-ktx:1.7.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.4.0"
+
+        object Compose {
+            const val version = "1.0.5"
+
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+            const val uiTestManifest = "androidx.compose.ui:ui-test-manifest:$version"
+        }
+
+        object Room {
+            private const val version = "2.3.0"
+            const val runtime = "androidx.room:room-runtime:$version"
+            const val kapt = "androidx.room:room-compiler:$version"
+            const val coroutine = "androidx.room:room-ktx:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.4.0-rc01"
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
+            const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.4.0"
+            const val core = "androidx.test:core:$version"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object Hilt {
+        private const val version = "2.39"
+
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val android = "com.google.dagger:hilt-android:$version"
+        const val compiler = "com.google.dagger:hilt-compiler:$version"
+        const val testing = "com.google.dagger:hilt-android-testing:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val gsonConverter = "com.squareup.retrofit2:converter-gson:$version"
+    }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
+    object Coil {
+        const val coilCompose = "io.coil-kt:coil-compose:1.3.2"
+    }
+
+}
